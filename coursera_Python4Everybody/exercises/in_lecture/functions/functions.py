74a8ff5bbd97df35a3308ac952609cb5286d2f5a
rakesh <rakeshcs.2000@gmail.com>
2019-12-28 23:56:24 +0530

    practice1

---
+++ coursera_Python4Everybody/exercises/in_lecture/functions/functions.py
@@ -0,0 +1,5 @@
+'''write a python program to take a string and an integer input and find the max element in both of them'''
+#print the max character
+str1 = str(input('Enter a word:'))
+print(max(str1))
+print(min(str1))
